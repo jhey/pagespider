dbdf7b9cf9287ad1411083e7ca25974876048862
TMH\tmhjxr49 <jrodriguez@houstonmethodist.org>
2017-07-17 15:37:00 -0500

    Initial commit

---
+++ utilities/database_utilities.py
@@ -0,0 +1,7 @@
+def create_database(database_path: str):
+    #TODO generate the db
+    pass
+
+def save_words_to_database(database_path: str):
+    #TODO: save words to db
+    pass
---
+++ utilities/url_utilities.py
@@ -0,0 +1,12 @@
+def load_urls_from_file(file_path: str):
+    #TODO: read txt file, with URL inside
+    pass
+
+
+def load_page(url: str):
+    #TODO: read the urls
+    pass
+
+def scrape_page(page_contents: str):
+    #TODO analyze text
+    pass
